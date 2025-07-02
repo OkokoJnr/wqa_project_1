37554832608121fdafb99df9e77efe7d48b114c9
OkokoJnr <96591819+OkokoJnr@users.noreply.github.com>
2025-07-02 09:21:22 +0100

    Transform price_usd column by removing $ and , and converting to float

--- Practice.py
+++ Practice.py
@@ -4,4 +4,13 @@ df2 = pd.read_csv('mexico-real-estate-2.csv')
 #Cleaning df1
 # 1. Remove NaN values
 df1.dropna(inplace=True)
+# 2. Transform price_usd column: Remove "$" and "," and convert to float
+
+df1["price_usd"] = (
+    df1["price_usd"]
+    .str.replace("$", "") #remove dollar sign
+    .str.replace(",", "") #remove commas
+    .astype(float)) #convert to float
+
+print(df1["price_usd"])
 print(df1.info())
