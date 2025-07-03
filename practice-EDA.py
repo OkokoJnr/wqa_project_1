a0e1281de113d1a9a5074599d45f61ba745b63ea
OkokoJnr <96591819+OkokoJnr@users.noreply.github.com>
2025-07-03 13:44:56 +0100

    update dataset

--- practice-EDA.py
+++ practice-EDA.py
@@ -11,27 +11,28 @@ pio.renderers.default = "browser"
 df = pd.read_csv('cleaned_mexico_real_estate.csv')
 
 #Task 1: Using Scatter Mapbox plot
-fig = px.scatter_map(
-    df,
-    lat='lat',
-    lon='lon',
-    center={"lat":19.43, "lon":-99.13},  # Center on Mexico City
-    width=1000,
-    height=800,
-    hover_data=['price_usd', 'state'],
-)
+# fig = px.scatter_map(
+#     df,
+#     lat='lat',
+#     lon='lon',
+#     center={"lat":19.43, "lon":-99.13},  # Center on Mexico City
+#     width=1000,
+#     height=800,
+#     hover_data=['price_usd', 'state'],
+# )
 #Add mapbox style to figure layout
 #fig.update_layout(mapbox_style="carto-positron")
-fig.update_layout(
-    mapbox_style="open-street-map",  # Use OpenStreetMap style
-    mapbox_zoom=5,  # Set zoom level
-    mapbox_center={"lat": 19.43, "lon": -99.13}  # Center on Mexico City
+# fig.update_layout(
+#     mapbox_style="open-street-map",  # Use OpenStreetMap style
+#     mapbox_zoom=5,  # Set zoom level
+#     mapbox_center={"lat": 19.43, "lon": -99.13}  # Center on Mexico City
     
-)
-# Show the plot
-fig.show()
-# Save the figure as an image
-fig.write_image("images/mexico_map.png")
+# )
+# # Show the plot
+# fig.show()
+# # Save the figure as an image
+# fig.write_image("images/mexico_map.png")
+
 
 # Display the first few rows of the DataFrame
-print(df.info())
+print(df["state"].unique())
